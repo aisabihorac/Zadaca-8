a65e2a16513e07430c52f6446a85eaf09fb45c59
aisa <aisabihorac965@gmail.com>
2024-08-21 22:45:19 +0200

    Zadatak 1  - 5 bod

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Zadaca-8
+Ovo je zadaca 8.Ima tri zadatka i, jedan opionalni.
---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let image = document.querySelector('img');
+    image.addEventListener('click', function() {
+        alert('Ovo je slika palestinske masline!');
+    });
+});
